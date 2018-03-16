080c4dc26c4ec682d13839d1c0d6d90a94c7baf3
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-03-16 02:52:29 -0500

    add dependencies

---
+++ Concesionario_Lab2/Concesionario_Lab2/Concesionario_Lab2-web/src/main/java/com/udea/controller/AutomovilMBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author alejandro
+ */
+@Named(value = "automovilMBean")
+@SessionScoped
+public class AutomovilMBean implements Serializable{
+
+    /**
+     * Creates a new instance of AutomovilMBean
+     */
+    public AutomovilMBean() {
+    }
+    
+}
