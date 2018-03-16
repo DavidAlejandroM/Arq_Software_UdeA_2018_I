36017bafaf6cf7938ff3e441330a2e6bfc18cfa0
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-03-16 03:22:15 -0500

    add methods handler to automovilMBean

--- Concesionario_Lab2/Concesionario_Lab2/Concesionario_Lab2-web/src/main/java/com/udea/controller/AutomovilMBean.java
+++ Concesionario_Lab2/Concesionario_Lab2/Concesionario_Lab2-web/src/main/java/com/udea/controller/AutomovilMBean.java
@@ -5,7 +5,16 @@
  */
 package com.udea.controller;
 
+import com.udea.entity.Automovil;
+import com.udea.session.AutomovilManagerLocal;
+import com.udea.session.FacturaManagerLocal;
+import com.udea.session.LineaManagerLocal;
+import com.udea.session.MarcaManagerLocal;
+import com.udea.session.TipoAutomovilManagerLocal;
+import com.udea.session.VentaManagerLocal;
 import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -17,6 +26,67 @@ import javax.inject.Named;
 @SessionScoped
 public class AutomovilMBean implements Serializable{
 
+    @EJB
+    private VentaManagerLocal ventaManager;
+
+    @EJB
+    private TipoAutomovilManagerLocal tipoAutomovilManager;
+
+    @EJB
+    private MarcaManagerLocal marcaManager;
+
+    @EJB
+    private LineaManagerLocal lineaManager;
+
+    @EJB
+    private FacturaManagerLocal facturaManager;
+
+    @EJB
+    private AutomovilManagerLocal automovilManager;
+
+    
+    private Automovil automovil;
+    private List<Automovil> automoviles;
+    
+    public List<Automovil> getAutomoviles(){
+        if (automoviles == null || automoviles.isEmpty())
+            refresh();
+            return automoviles;
+        
+    }
+    
+    /**
+    * Action handler - llamado cuando en una fila de la tabla se haga click en ID
+    * @param customer
+    * @return
+    */
+    public String showDetails(Automovil automovil) {
+        this.automovil = automovil;
+        return "DETAILS"; // Permite enlazar a CustomerDetails.xml 
+    }
+    
+    /**
+    * Action handler - Actualiza el modelo Customer en la BD.
+    * Se llama cuando se presiona el boton update del formulario
+    * @return
+    */
+    public String update() {
+        System.out.println("###UPDATE###");
+        automovil = automovilManager.update(automovil);
+        return "SAVED"; // Para el caso navegacional
+    }
+    /**
+    * Action handler - returno hacia la vista de la lista de clientes
+    * @return
+    */
+    public String list() {
+        System.out.println("###LIST###");
+        return "LIST";
+    }
+    
+    private void refresh() {
+        automoviles = automovilManager.getAllAutomoviles();
+    }
     /**
      * Creates a new instance of AutomovilMBean
      */
